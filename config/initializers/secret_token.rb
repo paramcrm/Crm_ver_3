7ff4cab9de73757468d0b36155e9db1b038bef57
kalpit patel <kalpit.p@gvocom.com>
2012-04-26 10:52:42 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmVer3::Application.config.secret_token = 'f5f06c78ec09f1debddc135397c222f9f04b0ea0a4a1f037ccf5404c362fda644d955b7df264982f03edcf447474ba519f00208d2941f529471046ff9a96a01f'
